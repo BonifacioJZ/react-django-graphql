d6a91ec4ff2faed85096ec4f16518ff877f7e1ca
Bonifacio Juarez Ceja <revanjz@gmail.com>
2019-09-13 05:47:19 -0500

    Base de datos Postgres sql

--- example/example/settings.py
+++ example/example/settings.py
@@ -89,10 +89,20 @@ WSGI_APPLICATION = 'example.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movie',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+
 }
 
 
